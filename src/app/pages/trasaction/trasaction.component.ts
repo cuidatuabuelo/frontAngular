c6399870ce7008452874e6db6f4b72735105158d
Nicolas Arias Sabogal <narias.as@gmail.com>
2019-09-28 17:20:47 -0500

    Final commit

--- src/app/pages/trasaction/trasaction.component.ts
+++ src/app/pages/trasaction/trasaction.component.ts
@@ -20,8 +20,16 @@ export class TrasactionComponent implements OnInit {
   serviceId: number;
   serviceName: string;
 
+  modelData: any[] = [];
+
+
+  selectedProduct: any = {};
+  selectedUser: any = {};
+  selectedService: any = {};
+
+
   constructor(private api: RestService) { }
-  
+
 
   ngOnInit() {
     this.api.get('/users').then(response => {
@@ -38,15 +46,27 @@ export class TrasactionComponent implements OnInit {
   }
 
   createTransaction() {
-    this.api.post('/transactions', { userId: 2, productId: this.productId,
-       serviceId: this.serviceId, initialDate:  this.initialDate, finalDate: this.initialDate }).then( response =>{
-        console.log(response);
+    this.api.post('/transactions', {
+      userId: 3, productId: this.productId,
+      serviceId: this.serviceId, initialDate: this.initialDate, finalDate: this.initialDate
+    }).then(response => {
+      alert(response.message);
+      $('#exampleModal').modal('hide');
     });
   }
 
-  openModal(serviceId: number, serviceName: string) {
+  async openModal(serviceId: number, id: number) {
     this.serviceId = serviceId;
-    this.serviceName = serviceName;
+;
+
+    this.selectedProduct = this.products.filter(f => f.productId == this.productId)[0];
+    this.selectedService = this.services.filter(f => f.serviceId === this.serviceId)[0];
+    this.selectedUser = this.users.filter(f => f.id === 2)[0];
+
+    console.log(this.selectedProduct);
+    console.log(this.selectedService);
+    console.log(this.selectedUser);
+
     $('#exampleModal').modal('show');
   }
 
