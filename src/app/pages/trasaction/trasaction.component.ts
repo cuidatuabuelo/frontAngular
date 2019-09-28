739890fe02b5a022bed79ef637e94a0074aad9ac
Jefferson Roa <jeffer.roabeltran@gmail.com>
2019-09-28 16:03:12 -0500

    add services transaction

--- src/app/pages/trasaction/trasaction.component.ts
+++ src/app/pages/trasaction/trasaction.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../services/rest.service';
 
 @Component({
   selector: 'app-trasaction',
   templateUrl: './trasaction.component.html',
   styleUrls: ['./trasaction.component.css']
 })
-export class TrasactionComponent implements OnInit {
 
-  constructor() { }
+export class TrasactionComponent implements OnInit {
+  users: any = [];
+  services: any = [];
+  constructor(private api: RestService) { }
+  
 
   ngOnInit() {
+    this.api.get('users').then(response => {
+      this.users = response;
+      console.log(this.users);
+    });
+    this.api.get('services').then(response => {
+      this.services = response;
+    });
   }
 
 }
